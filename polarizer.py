00e0f4a360c2ece7b6f46ba083e2b5b9fe2162cb
Gavin Dinubilo <gavin@reamaze.com>
2021-01-12 20:45:34 -0800

    Add in another render mode, Add in debug mode

--- polarizer.py
+++ polarizer.py
@@ -2,19 +2,27 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFilter, ImageEnhance
 import drawSvg as draw
 import argparse
-import mlrose
 from quadtree import QuadTree
 import NN
+import random
+from math import hypot
 
 parser = argparse.ArgumentParser()
 parser.add_argument("image", help="set image to polarize")
 parser.add_argument("capacity", type=int, help="set cell capacity")
 parser.add_argument("small", type=int, help="definition of a small cell")
+parser.add_argument("--mode", default="squiggle", choices=["line", "squiggle"], help="polarize in line mode")
+parser.add_argument("--debug", action="store_true", help="turn on debug mode")
 args = parser.parse_args()
 
 capacity = args.capacity
 image = args.image
 small = args.small
+mode = args.mode
+debug = args.debug
+
+if debug:
+  print("--- Debug Mode ---")
 
 im = Image.open(image)
 width, height = im.size
@@ -24,8 +32,12 @@ im = im.convert('1')
 im.save('dither.jpg')
 width, height = im.size
 
+print("------------")
 print("Image Size")
 print(im.size)
+print("Render Mode")
+print(mode)
+print("------------")
 a = np.asarray(im)
 
 qtree = QuadTree(0, 0, width, height, capacity, small)
@@ -60,27 +72,38 @@ lst = NN.tsp(points)
 
 print("Drawing SVG")
 d = draw.Drawing(width, height)
-# New pretty looking generator
-
-
-# Previous non pretty looking generator
-p = draw.Path(stroke_width=1, stroke='black', fill='none')
-print(lst[0])
 start_x, start_y = grouped_cells[lst[0]].center_point()
-p.M(start_x, start_y)
+color = 'black'
 for point in lst:
-  cell = grouped_cells[point]
   x, y = cell.center_point()
   w, h = cell.size()
-  corners = cell.corners()
+  distance = hypot(x - start_x, y - start_y)
+
+  if debug:
+    color = "#%06x" % random.randint(0, 0xFFFFFF)
+    print(color)
+    print(distance)
+
+  if (distance > 10):
+    start_x, start_y = x, y
+
+  p = draw.Path(stroke_width=1, stroke=color, fill='none')
+  cell = grouped_cells[point]
+  corners = cell.corners
   edges = cell.middle_edges()
 
-  for index, corner in enumerate(corners):
-    edge = edges[index]
-    p.Q(corner[0], corner[1], edge[0], edge[1])
+  p.M(start_x, start_y)
+  if mode == "line":
+    p.L(x, y)
+  elif mode == "squiggle":
+    corners = random.sample(list(cell.corners()), len(cell.corners()))
+    for index, corner in enumerate(corners):
+      edge = edges[index]
+      p.Q(corner[0], corner[1], edge[0], edge[1])
 
+  start_x, start_y = x, y
+  d.append(p)
 
-d.append(p)
 d.saveSvg('rendered.svg')
 
 rendered.show()
